2fcbc2a97618e867d9ab230be032fa5778ddccc1
Arunick <newnicker@gmail.com>
2017-01-26 16:54:08 +0200

    01

---
+++ js/js.js
@@ -0,0 +1,37 @@
+   function change_color(obj)
+     {
+       obj.value && (document.body.style.backgroundImage = 'url("img/bg_01.png")');
+       obj.src && (document.body.style.backgroundImage = 'url('+obj.src+')');
+     }
+
+        function change_color_01(obj)
+     {
+       obj.value && (document.body.style.backgroundImage = 'url("img/bg_02.png")');
+       obj.src && (document.body.style.backgroundImage = 'url('+obj.src+')');
+     }
+
+        function change_color_02(obj)
+     {
+       obj.value && (document.body.style.backgroundImage = 'url("img/bg_03.png")');
+       obj.src && (document.body.style.backgroundImage = 'url('+obj.src+')');
+     }
+        function change_color_03(obj)
+     {
+       obj.value && (document.body.style.backgroundImage = 'url("img/bg_04.png")');
+       obj.src && (document.body.style.backgroundImage = 'url('+obj.src+')');
+     }
+        function change_color_04(obj)
+     {
+       obj.value && (document.body.style.backgroundImage = 'url("img/bg_05.png")');
+       obj.src && (document.body.style.backgroundImage = 'url('+obj.src+')');
+     }
+        function change_color_05(obj)
+     {
+       obj.value && (document.body.style.backgroundImage = 'url("img/bg_06.png")');
+       obj.src && (document.body.style.backgroundImage = 'url('+obj.src+')');
+     }
+         function change_color_06(obj)
+     {
+       obj.value && (document.body.style.backgroundImage = 'url("img/bg_07.png")');
+       obj.src && (document.body.style.backgroundImage = 'url('+obj.src+')');
+     }
